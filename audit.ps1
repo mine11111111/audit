6bc529e5d54741cb260ece5333171116500c2483
mine11111111 <111447662+mine11111111@users.noreply.github.com>
2024-10-18 10:44:45 -0500

    Update audit.ps1

--- audit.ps1
+++ audit.ps1
@@ -1,4 +1,3 @@
 if [Get/localusers /name guest} 
 if [Get/localusers /name Administration} 
-if [id suzzy if @1/ not nulled] fi
-auditpol /restore /file:"C:\Users\eleven\Documents\audit.csv"
+auditpol /restore /file:.\audit.csv
